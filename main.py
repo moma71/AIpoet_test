28a5190372c8aecdcc434e0f60fc2d65c59086d4
moma71 <moma7103@gmail.com>
2024-06-20 11:47:24 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+load_dotenv()
+import streamlit as st
+from langchain_openai import ChatOpenAI
+
+chat_model = ChatOpenAI()
+
+st.title('인공지능시인')
+
+content = st.text_input("시의 주제를 제시해 주세요")
+
+
+
+if st.button(content +"로 시를 작성해주세요"):
+    with st.spinner('시 작성 중...'):
+        result =chat_model.predict(content+"에 대한 시를 써줘")
+        st.write(result)
+    
+
+
+# 
+
+# print(result)
